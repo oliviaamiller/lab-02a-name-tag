798e9c5dda908002e6d2fa91813cc2cea3230f2e
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-11-04 11:12:44 -0700

    added single color change button

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const changeInputEl = document.getElementById('change-input');
-
 const changeButtonEl = document.getElementById('change-button');
-
 const changeNameEl = document.getElementById('change-name');
+const newColorButtonEl = document.getElementById('color-button');
+const newColorBoxEl = document.getElementById('new-color-box');
 
 
 changeButtonEl.addEventListener('click', () => {
-
     changeNameEl.textContent = changeInputEl.value;
-
 });
+
+
+newColorButtonEl.addEventListener('click', () => {
+    newColorBoxEl.style.backgroundColor = "magenta";
+});
+ 
