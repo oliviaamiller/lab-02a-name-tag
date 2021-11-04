f1662088e67ff2efca667e23496b59b4b1e12be6
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-11-04 11:47:50 -0700

    added pronouns button

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const changeInputEl = document.getElementById('change-input');
 const changeButtonEl = document.getElementById('change-button');
 const changeNameEl = document.getElementById('change-name');
+
 const newColorButtonEl = document.getElementById('color-button');
 const newColorBoxEl = document.getElementById('new-color-box');
+
+const pronounsInputEl = document.getElementById('add-pronouns');
+const pronounsButtonEl = document.getElementById('pronouns-button');
+const prounounsHereEl = document.getElementById('pronouns-here');
+
+
 
 const colors = ['pink', 'red', 'orange', 'yellow', 'blue', 'green'];
 
@@ -19,3 +26,7 @@ newColorButtonEl.addEventListener('click', () => {
     colorNumber++;
 });
  
+
+pronounsButtonEl.addEventListener('click', () => {
+    prounounsHereEl.textContent = pronounsInputEl.value;
+});
