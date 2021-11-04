ddd3fdc0a69063585971a2e25651cb260f1e9b49
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-11-04 11:18:12 -0700

    added rotating color button!

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ const changeNameEl = document.getElementById('change-name');
 const newColorButtonEl = document.getElementById('color-button');
 const newColorBoxEl = document.getElementById('new-color-box');
 
+const colors = ['pink', 'red', 'orange', 'yellow', 'blue', 'green'];
+
 
 changeButtonEl.addEventListener('click', () => {
     changeNameEl.textContent = changeInputEl.value;
 });
 
 
+let colorNumber = 0;
+
 newColorButtonEl.addEventListener('click', () => {
-    newColorBoxEl.style.backgroundColor = "magenta";
+    newColorBoxEl.style.backgroundColor = colors[colorNumber % colors.length];   
+    colorNumber++;
 });
  
